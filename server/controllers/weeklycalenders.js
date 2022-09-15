2f677a2ad882046204233e3d51899f0f0a885513
Emma Litvin <guslitvem@student.gu.se>
2022-09-15 22:52:04 +0200

    Add GET for all weeklycalenders

--- server/controllers/weeklycalenders.js
+++ server/controllers/weeklycalenders.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var Weeklycalender = require('../models/weeklycalender');
+
 const weeklycalenderPath = '/api/profiles/:profileId/weeklycalenders';
 const specificWeeklycalenderPath = '/api/profiles/:profileId/weeklycalenders/:weeklycalenderId';
-var router = express.Router();
 
-var Weeklycalender = require('../models/weeklycalender');
+var router = express.Router();
 
 //Create weekly calender
 router.post(weeklycalenderPath,function(req, res,next) {
@@ -16,51 +17,11 @@ router.post(weeklycalenderPath,function(req, res,next) {
 
 //Get all weekly calenders
 router.get(weeklycalenderPath, function(req, res, next) {
-    Weeklycalender.find(function(err, weeklycalenders){
-      if(err) { return next(err); }
-      res.json({'weeklycalenders:': weeklycalenders});
-    });
-  });
-  
-//Get specific weekly calender
-router.get(specificWeeklycalenderPath, function(req, res, next) {
-  var id = req.params.id;
-  Weeklycalender.findOne(id, function(err, weeklycalender) {
-      if(err) { return next(err); }
-      if(weeklycalender === null) {
-        return res.status(404).json({ 'message' : 'The weekly calender not found' });
-      }
-      res.json(weeklycalender);
-    });
-  });
-
-//Update specific Weekly calender
-router.patch(specificWeeklycalenderPath, function(req, res, next) {
-  var id = req.params.id;
-  Weeklycalender.findById(id, function(err, weeklycalender) {
-     if (err) { return next(err); }
-     if (weeklycalender == null) {
-         return res.status(404).json({ "message" : "Weekly calender not found" });
-     }
-    weeklycalender.week = (req.body.week || weeklycalender.week);
-    weeklycalender.year = (req.body.year || weeklycalender.year);
-    weeklycalender.save();
-    res.json(weeklycalender);
- });
-});
-
-//Delete specific Weekly calender
-router.delete(specificWeeklycalenderPath, function(req, res, next) {
-  var id = req.params.id;
-  Weeklycalender.findOneAndDelete({_id: id}, function(err, weeklycalender) {
-      if (err) { return next(err); }
-      if (weeklycalender == null) {
-          return res.status(404).json({'message': 'Weekly calender not found'});
-      }
-      res.json(weeklycalender);
+  Weeklycalender.find(function(err, weeklycalenders){
+    if(err) { return next(err); }
+    res.json({'weeklycalenders:': weeklycalenders});
   });
 });
-
 
 
   module.exports = router;
