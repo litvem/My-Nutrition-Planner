3f0d4daf6f45b9221eae3ffc80c30b17403daf5c
Emma Litvin <guslitvem@student.gu.se>
2022-09-16 01:18:19 +0200

    Add POST for days

--- server/controllers/days.js
+++ server/controllers/days.js
@@ -1,6 +1,18 @@
 var express = require('express');
+const Day = require('../models/day');
+
+const dayPath = '/api/profiles/:profileId/weeklycalenders/:weeklycalenderId/days';
+const specificDayPath = '/api/profiles/:profileId/weeklycalenders/:weeklycalenderId/days/:dayId';
+
 var router = express.Router();
 
+//Create day
+router.post(dayPath, function(req, res, next) {
+    var day = new Day(req.body);
+    day.save(function(err, day) {
+        if(err) {return next(err); }
+        res.status(201).json(day);
+    })
+});
 
-// code must go above this
 module.exports = router; 
