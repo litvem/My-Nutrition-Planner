29f844699f1c3658c684a8e22255e13dcc336691
Emma Litvin <guslitvem@student.gu.se>
2022-09-21 16:25:11 +0200

    Modify DAY schema and controller

--- server/models/day.js
+++ server/models/day.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 var daySchema = new Schema({
-    name: {type:String},
-    recipes:{type: [Schema.Types.ObjectId], ref:'recipes'}
+    name: { type: String ,
+        enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']},
+    week: { type: Number },
+    year: { type: Number },
+    recipes:{ type: [Schema.Types.ObjectId], ref:'recipes'}
     // restriction 5 recipes in the front end.
 });
 
