ce2a2d7b8ada87a30d19d9584face7958aba0d80
istrate <istrate@net.chalmers.se>
2022-09-23 12:37:31 +0200

    Add Login and Forgot password components and routing

--- client/src/views/ForgotPassword.vue
+++ client/src/views/ForgotPassword.vue
@@ -1,7 +1,35 @@
 <template>
     <div>
         <h1>Forgot password</h1>
-        <button class="btn" v-on:click="goToUserHome">Reset password</button>
+
+        <form action="#">
+            <div class="input-box">
+                <input type="text" placeholder="Enter your username" required>
+            </div>
+
+            <select class="form-select form-select-sm" aria-label=".form-select-sm example" required>
+                <option selected>Select your secret question</option>
+                <option value="1">What is the name of your first pet?</option>
+                <option value="2">When did your parents meet?</option>
+                <option value="3">What is your favorite season?</option>
+            </select>
+
+            <div class="input-box">
+                <input type="text" placeholder="Answer secret question" required>
+            </div>
+
+            <div class="input-box">
+                <input type="password" placeholder="Enter new password" required>
+            </div>
+
+            <div class="input-box">
+                <input type="password" placeholder="Confirm new password" required>
+            </div>
+
+            <div class="input-box">
+              <input type="submit" class="submit" v-on:click="goToUserHome" value="Reset password">
+            </div>
+        </form>
     </div>
 </template>
 
@@ -20,6 +48,6 @@ export default {
 
 <style scoped>
     h1 {
-        color:blue
+        color:black
     }
 </style>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
-       <h1>Login</h1>
-       <button class="btn" v-on:click="goToUserHome">Login</button>
+      <h1>Login</h1>
+        <form action="#">
+          <div class="container">
+            <input type="username" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+
+            <input type="submit" v-on:click="goToUserHome" value="Login">
+
+            <div class="bottom-container">
+              <div class="row">
+                <div class="col">
+                  <a href="#" v-on:click="goToForgotPassword" style="color:white" class="btn">Forgot password?</a>
+               </div>
+              </div>
+            </div>
+          </div>
+        </form>
     </div>
 </template>
 
@@ -11,6 +26,9 @@ export default {
   methods: {
     goToUserHome() {
       this.$router.push('/userHome')
+    },
+    goToForgotPassword() {
+      this.$router.push('/forgotPassword')
     }
   }
 }
@@ -18,9 +36,9 @@ export default {
 
 <style scoped>
     h1 {
-        color:blue
+        color:black
     }
     .btn {
-        background-color: turquoise;
+        background-color: gray;
     }
 </style>
