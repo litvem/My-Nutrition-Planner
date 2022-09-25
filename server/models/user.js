e47b700f9846ce894e31c9f65f811b192feb0cf2
Nicole <gusquini@student.gu.se>
2022-09-25 20:53:57 +0200

    added encrypted password for the user and token creation and updated postman user test

--- server/models/user.js
+++ server/models/user.js
@@ -2,12 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    _id:{type: String},
-    password: { type: String,
+    username:{type: String,
         required: true},
-    question:{type: String,
-        required: true}, 
-    answer:{type: String,
+    password: { type: String,
         required: true},
     recipes: {type: [Schema.Types.ObjectId], ref: 'recipes'},
     days:{type: [Schema.Types.ObjectId], ref:'days'},
