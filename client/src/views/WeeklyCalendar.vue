2b63d344db85a79aeae77c8448130e4835127b4b
Emma Litvin <guslitvem@student.gu.se>
2022-09-30 00:36:38 +0200

    Layout changes to WEEKLY CALENDAR

--- client/src/views/WeeklyCalendar.vue
+++ client/src/views/WeeklyCalendar.vue
@@ -1,7 +1,72 @@
 <template>
-    <div>
-       <h1>Weekly caledar</h1>
-    </div>
+    <b-container class="layout">
+        <b-row class="text-center">
+            <b-col align-self="stretch">
+                <br><h1>Weekly caledar</h1><br>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col class="text-center">
+                <h4>Monday</h4>
+            </b-col>
+            <b-col class="text-center">
+                <h4>Tuesday</h4>
+            </b-col>
+            <b-col class="text-center">
+                <h4>Wednesday</h4>
+            </b-col>
+            <b-col class="text-center">
+                <h4>Thursday</h4>
+            </b-col>
+            <b-col class="text-center">
+                <h4>Friday</h4>
+            </b-col>
+            <b-col class="text-center">
+                <h4>Saturday</h4>
+            </b-col>
+            <b-col class="text-center">
+                <h4>Sunday</h4>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+            <b-col>
+              <b-jumbotron>
+                <b-button pill variant="outline-secondary">+</b-button>
+              </b-jumbotron>
+            </b-col>
+        </b-row>
+
+    </b-container>
 </template>
 
 <script>
@@ -12,6 +77,12 @@ export default {
 
 <style scoped>
     h1 {
-        color:blue
+        color: #210b85
     }
+    .jumbotron {
+    min-width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: top;
+  }
 </style>
