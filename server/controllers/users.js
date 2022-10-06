0d3a8ac47a2b0039cb96498e84cc47134b691757
Nicole <gusquini@student.gu.se>
2022-10-06 21:02:03 +0200

    adding the return of the id from the login

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -96,7 +96,8 @@ router.post("/api/profiles/login", (req, res, next) => {
           );
           return res.status(200).json({
             message: "Authentication successful",
-            token: token
+            token: token,
+            id:user.id
           });
         }
         res.status(401).json({
