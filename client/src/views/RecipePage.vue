210771108dd239e6ecadf546472e6b51dfae47ef
istrate <istrate@net.chalmers.se>
2022-10-10 19:37:53 +0200

    Add RecipeTemplate, add logic to bind data from parent to child component

--- client/src/views/RecipePage.vue
+++ client/src/views/RecipePage.vue
@@ -1,80 +1,31 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <h1>Recipe Name</h1>
-    </div>
-
-    <div class="row">
-      <div class="col-6" id="left">
-
-        <div class="row">
-          <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
-        </div>
-
-        <br>
-
-          <div class="row">
-            <div class="col">
-              <div class="alert alert-success" role="alert"> <a href="#" class="alert-link">Category</a></div>
-            </div>
-          </div>
-
-          <div class="row">
-                  <div class="col">
-                    <b-button v-b-toggle.collapse-1 variant="primary">Add to Plan</b-button>
-                    <b-collapse id="collapse-1" class="mt-2">
-                        <b-card>
-                        <p class="card-text">Choose week:</p>
-                        <b-form-group v-slot="{ ariaDescribedby }">
-                        <b-form-checkbox-group
-                        v-model="selected"
-                        :options="options"
-                        :aria-describedby="ariaDescribedby"
-                        name="radios-stacked"
-                        stacked>
-                        </b-form-checkbox-group>
-                        </b-form-group>
-                    </b-card>
-                    </b-collapse>
-                  </div>
-                  <div class="col">
-                    <b-button variant="danger">Delete recipe</b-button>
-                  </div>
-
-        </div>
-    </div>
-    <div class="col-6" id="right">
-            <b-card>
-                <h4>Ingredients</h4>
-                <ul>
-                  <li>150 grams eggs</li>
-                </ul>
-                <br>
-
-                <h4>Instructions</h4>
-                <p>Heat oven to 250 Celsius</p>
-                <p>Bring water to boil</p>
-                <p>Add pasta</p>
-            </b-card>
-        </div>
-      </div>
-
-        </div>
+  <RecipeTemplate v-bind="recipe.recipe"/>
 </template>
 
 <script>
+import { Api } from '@/Api'
 export default {
   name: 'recipe-page',
+
+  mounted() {
+    // Api.get('/profiles/' + localStorage.id + '/recipes/' + this.$route.param.id)
+    Api.get('/profiles/' + localStorage.id + '/recipes/' + '6343d4d651b49c3d28bc85c7')
+      .then(response => {
+        console.log(response.data)
+        this.recipe = response.data
+      })
+      .catch(error => {
+        this.message = error.message
+        console.log(error.message)
+      })
+  },
   data() {
     return {
-      selected: 'first',
-      options: [
-        { text: '2022-Week 39', value: '39' },
-        { text: '2022-Week 40', value: '40' }
-      ]
+      recipe: null
     }
   }
 }
+
 </script>
 
 <style scoped>
