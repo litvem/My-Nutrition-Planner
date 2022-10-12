e98f812d4d073cb328f359c2009113b04f643556
istrate <istrate@net.chalmers.se>
2022-10-12 19:30:34 +0200

    add ShoppingList view with behaviour

--- client/src/views/ShoppingList.vue
+++ client/src/views/ShoppingList.vue
@@ -1,17 +1,56 @@
 <template>
-    <div>
-       <h1>Shopping List</h1>
+    <div class="container-fluid" id="shoppingListView">
+      <h2>Shopping lists</h2>
+        <b-button v-on:click="goToAddShoppingList">Create Shopping List</b-button>
+      <br>
+    <div class="card">
+     <div class="card-body">
+        <shopping-list-prev v-for="shoppingList in shoppingLists.shoppinglists"
+        :key="shoppingList.name"
+        :name="shoppingList.name" />
+     </div>
+   </div>
     </div>
 </template>
 
 <script>
+import ShoppingListPrev from '../components/ShoppingListPrev.vue'
+import { Api } from '@/Api'
+
 export default {
-  name: 'shoppingList'
+  name: 'shoppingList',
+  components: {
+    ShoppingListPrev
+  },
+  data() {
+    return {
+      message: '',
+      shoppingLists: null
+    }
+  },
+  beforeCreate() {
+    Api.get('/profiles/' + localStorage.id + '/shoppinglists')
+      .then(response => {
+        this.shoppingLists = response.data
+        console.log(response.data)
+        // console.log(Object.toString(response.data))
+        // console.log(Object.toString(this.shoppingLists))
+      })
+      .catch(error => {
+        this.message = error
+      })
+  },
+  methods: {
+    goToAddShoppingList() {
+      this.$router.push('/addShoppingList')
+    }
+  }
 }
 </script>
 
 <style scoped>
-    h1 {
-        color:blue
-    }
+  #shoppingListView{
+    background-image: url('https://img.freepik.com/free-photo/condiments-prepare-italian-pasta_1220-429.jpg?w=996&t=st=1664979691~exp=1664980291~hmac=3f8d79f6fc3ee5a911438a9d9514716e6021d1956446eb0a438fcdce8088ec3d');
+    background-size: cover;
+  }
 </style>
