1ce0ce39ecd2f4d9592d12202e614e97e66a82f6
Emma Litvin <guslitvem@student.gu.se>
2022-10-16 16:17:31 +0200

    Add button 'Cancel' for add new weekly plan form + CSS fixes

--- client/src/views/AllPlans.vue
+++ client/src/views/AllPlans.vue
@@ -7,9 +7,17 @@
         </div>
         <div id="container-left">
          <div class="list" >
-          <!-- HERE IS THE LOGIC FOR THE NO WEEK PLANS -->
-          <h4 v-show="plans.length === 0"> There are no Weekly Plans yet</h4>
-          <!-- AT LEAST 1 WEEK PLANS -->
+            <b-row id="alert-holder" v-show="plans.length === 0">
+              <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </symbol>
+              </svg>
+              <div class="alert alert-warning d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                  You have no Weekly plans yet.
+              </div>
+          </b-row>
           <b-row v-show="plans.length > 0">
             <b-col cols="12" lg="6" sm="12" v-for="plan in plans" v-bind:key="plan._id">
               <h5 v-on:click="goToWeeklyPlan()">Week {{plan.week}}  Year {{plan.year}}</h5>
@@ -28,11 +36,12 @@
           <div class="name" v-if="addPlan">
             <b-form-select v-model="name" :options="options" v-if="addPlan"></b-form-select>
           </div>
-          <label for="week-number" v-if="addPlan">Week number: </label>
+          <label for="week-number" v-if="addPlan">Week number:</label>
           <b-form-input id="week" v-model="week" v-if="addPlan"/>
-          <label for="year" v-if="addPlan">Year: </label>
+          <label for="year" v-if="addPlan">Year:</label>
           <b-form-input id="year" v-model="year" v-if="addPlan" />
           <button class="save-plan" v-on:click="savePlan" v-if="addPlan">Save</button>
+          <button class="cancel-form" @click="cancelSavePlan">Cancel</button>
         </div>
       </div>
     </div>
@@ -104,6 +113,9 @@ export default {
           console.error(error)
         })
     },
+    cancelSavePlan() {
+      this.addPlan = false
+    },
     deleteAllPlans() {
       Api.delete('/profiles/' + localStorage.id + '/days', {
         headers: {
@@ -114,7 +126,7 @@ export default {
           console.log(response)
         })
         .catch(error => {
-          this.message = error
+          console.error(error)
         })
       this.$router.go()
     }
@@ -141,24 +153,6 @@ export default {
     width: 61%;
   }
 
-  .right {
-    width: 39%;
-  }
-
-  .plan-form{
-    background-color: rgba(255, 255, 255, 0.468);
-    margin-top: 57%;
-    margin-left: 10%;
-    margin-right: 10%;
-    height: 40%;
-    width: 80%;
-    padding: 13px 20px;
-    border-radius: 8px;
-    display: inline-block;
-    border: 0;
-    outline: 0;
-    box-shadow: 0px 4px 10px 0px #bcff0375;
-  }
   .left .title {
     height: 100%;
     display: flex;
@@ -203,14 +197,9 @@ export default {
     text-align: left;
   }
 
-  h4{
-    color: #fff;
-  }
-
-  .plan-form {
-    color: rgb(71, 41, 2);
-    font-size: 16px;
-    font-weight: bold;
+  #alert-holder {
+    margin-left: 1%;
+    margin-right: 25%;
   }
 
   .buttons {
@@ -238,7 +227,7 @@ export default {
     font-weight: bold;
     text-decoration: underline;
     box-shadow: 0px 4px 10px 0px #ebfbd985;
-    background-image: linear-gradient(135deg, #88a40e89 10%, #3d5403ad 100%);
+    background-image: linear-gradient(135deg, #9fad03e9 10%, #304903de 100%);
     }
   }
 
@@ -258,14 +247,35 @@ export default {
     outline: 0;
     box-shadow: 0px 4px 10px 0px #f9a3548e;
     background-image: linear-gradient(135deg, #f6b974ca 10%, #ff8000c7 100%);
+    &:hover {
+      color: #fff;
+      font-weight: bold;
+      text-decoration: underline;
+      box-shadow: 0px 4px 10px 0px #fec7938e;
+      background-image: linear-gradient(135deg, #e87c02f3 10%, #442301 100%);
+    }
   }
 
-  .delete-btn:hover {
-    color: #fff;
+  .right {
+    width: 39%;
+  }
+
+  .plan-form{
+    background-color: rgba(255, 255, 255, 0.468);
+    margin-top: 57%;
+    margin-left: 10%;
+    margin-right: 10%;
+    height: 40%;
+    width: 80%;
+    padding: 13px 20px;
+    border-radius: 8px;
+    display: inline-block;
+    border: 0;
+    outline: 0;
+    box-shadow: 0px 4px 10px 0px #bcff0375;
+    color: rgb(71, 41, 2);
+    font-size: 16px;
     font-weight: bold;
-    text-decoration: underline;
-    box-shadow: 0px 4px 10px 0px #fec7938e;
-    background-image: linear-gradient(135deg, #bb6605ca 10%, #442201c7 100%);
   }
 
   .save-plan {
@@ -284,14 +294,38 @@ export default {
     outline: 0;
     box-shadow: 0px 4px 10px 0px #85db0d8e;
     background-image: linear-gradient(135deg, #eff886a3 10%, #a6ff0080 100%);
+    &:hover {
+      color: #fff;
+      font-weight: bold;
+      text-decoration: underline;
+      box-shadow: 0px 4px 10px 0px #4324018e;
+      background-image: linear-gradient(135deg, #9fad03e9 10%, #304903de 100%);
+    }
   }
 
-  .save-plan:hover {
-    color: #fff;
-    font-weight: bold;
-    text-decoration: underline;
-    box-shadow: 0px 4px 10px 0px #4324018e;
-    background-image: linear-gradient(135deg, #9fad03a3 10%, #31490380 100%);
+  .cancel-form {
+    margin-top: 0.7em;
+    margin-bottom: 0.7em;
+    margin-right: 0;
+    margin-left: 1em;
+    float: center;
+    align-self: auto;
+    color: black;
+    font-size: 16px;
+    padding: 12px 25px;
+    border-radius: 50px;
+    display: inline-block;
+    border: 0;
+    outline: 0;
+    box-shadow: 0px 4px 10px 0px #db7e0d8e;
+    background-image: linear-gradient(135deg, #f8a486a3 10%, #ff330080 100%);
+    &:hover {
+      color: #fff;
+      font-weight: bold;
+      text-decoration: underline;
+      box-shadow: 0px 4px 10px 0px #f89f9f8e;
+      background-image: linear-gradient(135deg, #ad1a03d2 10%, #470000 100%);
+    }
   }
 
   h5 {
@@ -310,13 +344,12 @@ export default {
     outline: 0;
     box-shadow: 0px 4px 10px 0px #f7eca57c;
     background-image: linear-gradient(135deg, #fcf4035c 10%, #ffcd0449 100%);
-  }
-
-  h5:hover {
-    color: rgb(66, 26, 1);
-    text-decoration: underline;
-    background-color: rgba(255, 152, 96, 0.345);
-    box-shadow: 0px 4px 10px 0px #482601a0;
+    &:hover {
+      color: rgb(66, 26, 1);
+      text-decoration: underline;
+      background-color: rgba(255, 152, 96, 0.345);
+      box-shadow: 0px 4px 10px 0px #482601a0;
+    }
   }
 
   @media(max-width: 768px) {
@@ -335,7 +368,7 @@ export default {
       }
     }
 
-    h1{
+    h1 {
       font-size: 30px;
 
     }
