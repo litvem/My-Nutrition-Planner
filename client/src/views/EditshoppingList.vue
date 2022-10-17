c5795216eb7080d9dd4b9b8cf2f47d34d92e82ab
istrate <istrate@net.chalmers.se>
2022-10-17 05:50:15 +0200

    add buttons and logic for add/remove row of items in edit SL, input validation, error handling, improve patch

--- client/src/views/EditshoppingList.vue
+++ client/src/views/EditshoppingList.vue
@@ -29,17 +29,33 @@
         </b-button>
         <br>
 
-    <b-row aligh-h="center" v-for="item in shoppinglist[0].items" :key="item._id">
+    <b-row aligh-h="center" v-for="(item,index) in shoppinglist[0].items" :key="index">
       <b-col>
         <b-form-input :disabled="!editList" type="number" name="item" v-model="item.amount" class="form-control"></b-form-input>
       </b-col>
       <b-col>
-        <b-form-input :disabled="!editList" type="text" name="unit" v-model="item.unit" class="form-control"></b-form-input>
+        <b-form-select :disabled="!editList" type="text" name="unit" v-model="item.unit" class="form-control" placeholder="Unit">
+          <option value="pcs">pcs</option>
+          <option value="grams">grams</option>
+          <option value="kg">kg</option>
+          <option value="ml">ml</option>
+          <option value="dl">dl</option>
+          <option value="l">l</option>
+        </b-form-select>
       </b-col>
       <b-col>
         <b-form-input :disabled="!editList" type="text" name="amount" v-model="item.item" class="form-control"></b-form-input>
       </b-col>
       </b-row>
+      <div class="d-flex justify-content-center">
+       <b-button-group style="margin-top: 5px;">
+        <b-button variant="info" class="menu-button"  @click="add">
+          <b-icon icon="plus"></b-icon> Add item</b-button>
+        <br>
+        <b-button variant="danger" class="menu-button" @click="remove">
+          <b-icon icon="x"></b-icon> Remove Item</b-button>
+       </b-button-group>
+      </div>
     </div>
  </div>
 </div>
@@ -55,7 +71,10 @@ export default {
     return {
       shoppinglist: null,
       editName: false,
-      editList: false
+      editList: false,
+      count: null,
+      editedList: [],
+      isValid: false
     }
   },
 
@@ -68,65 +87,95 @@ export default {
       .then(response => {
         this.shoppinglist = response.data.shoppinglist
         this.count = this.shoppinglist.length
-        this.shoppinglist.forEach(sl => {
-          sl.items.forEach(item => {
-            console.log(item.unit)
-          })
-        })
       })
   },
   methods: {
+    add: function () {
+      this.shoppinglist[0].items.push({ amount: null, unit: '', item: '' })
+      this.count++
+    },
+    remove: function () {
+      if (this.shoppinglist[0].items.length > 1) {
+        this.shoppinglist[0].items.pop()
+        this.count--
+      } else {
+        alert('Warning: shopping list cannot have less than 1 item!')
+      }
+    },
     goToShoppingLists() {
       this.$router.push('/shoppingLists')
     },
+    checkInput: function () {
+      if (this.editedList.length < 1) {
+        alert('Please add at least one item in your shopping list!')
+        this.isValid = false
+      } else if (this.shoppinglist[0].name === '') {
+        alert('Please give your shopping list a name!')
+        this.isValid = false
+      } else {
+        this.isValid = true
+      }
+    },
     saveShoppingList() {
       if (this.editName === true && this.editList === false) {
-        Api.put('/profiles/' + localStorage.id + '/shoppingLists/' + this.$route.params.id, {
-          profileId: localStorage.id,
-          name: this.shoppinglist[0].name,
-          week: this.shoppinglist[0].week,
-          day: this.shoppinglist[0].day,
-          items: this.shoppinglist[0].items
-        },
-        {
-          headers: {
-            Authorization: 'Bearer ' + localStorage.getItem('token')
-          }
-        }).then(response => {
-          if (response.status === 200) {
-            alert('Shoppinglist name was edited with succes!')
-            this.$router.push('/shoppingLists')
-          }
-        }).catch(error => {
-          console.log(error)
-        })
-        // TODO catch
-      } else if (this.editList === true) {
-        for (let i = 0; i < this.count; i++) {
-          if (this.shoppinglist[0].items.amount <= 0 && this.shoppinglist[0].items.unit === '' && this.shoppinglist[0].items.item === '') {
-            // TODO remove null item
-          }
+        if (this.shoppinglist[0].name === '') {
+          alert('Please give your shopping list a name!')
+        } else {
+          Api.put('/profiles/' + localStorage.id + '/shoppingLists/' + this.$route.params.id, {
+            profileId: localStorage.id,
+            name: this.shoppinglist[0].name,
+            week: this.shoppinglist[0].week,
+            day: this.shoppinglist[0].day,
+            items: this.shoppinglist[0].items
+          },
+          {
+            headers: {
+              Authorization: 'Bearer ' + localStorage.getItem('token')
+            }
+          }).then(response => {
+            if (response.status === 200) {
+              alert('Shoppinglist name was edited with succes!')
+              this.$router.push('/shoppingLists')
+            }
+          }).catch(error => {
+            console.log(error)
+            alert('Warning: Could not save shopping list.')
+          })
         }
-        // TODO check if array has elements
-        Api.patch('/profiles/' + localStorage.id + '/shoppingLists/' + this.$route.params.id, {
-          profileId: localStorage.id,
-          name: this.shoppinglist[0].name,
-          week: this.shoppinglist[0].week,
-          day: this.shoppinglist[0].day,
-          items: this.shoppinglist[0].items
-        },
-        {
-          headers: {
-            Authorization: 'Bearer ' + localStorage.getItem('token')
-          }
-        }).then(response => {
-          if (response.status === 200) {
-            alert('Shoppinglist was edited with succes!')
-            this.$router.push('/shoppingLists')
-          }
+      } else if (this.editList === true) {
+        this.shoppinglist.forEach(sl => {
+          sl.items.forEach(item => {
+            if (item.amount !== null && item.unit !== '' && item.item !== '') {
+              this.editedList.push(item)
+              console.log(this.editedList)
+            }
+          })
         })
+
+        this.checkInput()
+
+        if (this.isValid) {
+          Api.patch('/profiles/' + localStorage.id + '/shoppingLists/' + this.$route.params.id, {
+            profileId: localStorage.id,
+            name: this.shoppinglist[0].name,
+            week: this.shoppinglist[0].week,
+            day: this.shoppinglist[0].day,
+            items: this.editedList
+          },
+          {
+            headers: {
+              Authorization: 'Bearer ' + localStorage.getItem('token')
+            }
+          }).then(response => {
+            if (response.status === 200) {
+              alert('Shoppinglist was edited with succes!')
+              this.$router.push('/shoppingLists')
+            }
+          })
+        }
       }
     }
+
   }
 }
 </script>
