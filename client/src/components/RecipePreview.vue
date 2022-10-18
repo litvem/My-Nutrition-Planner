c4931afded03c033570c2aa165a609d1cd31961e
Emma Litvin <guslitvem@student.gu.se>
2022-10-18 13:32:05 +0200

    Remove duplicated category from RecipePreview

--- client/src/components/RecipePreview.vue
+++ client/src/components/RecipePreview.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <img id="image" class="img" :src= "recipe.image" />
       <div class="content">
-        <h5 v-on:click="goToRecipePage()" v-bind="recipe">{{recipe.name}}   {{recipe.category}}</h5>
+        <h5 v-on:click="goToRecipePage()" v-bind="recipe">{{recipe.name}}</h5>
         <h6>Category: {{ recipe.category }}</h6>
       </div>
     </div>
