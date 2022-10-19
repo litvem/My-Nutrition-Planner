7989b232e2ba54b90cd72d16d3b1916cade1473a
istrate <istrate@net.chalmers.se>
2022-10-20 01:16:59 +0200

    post and patch work when pressing button twice

--- client/src/views/RecipePage.vue
+++ client/src/views/RecipePage.vue
@@ -73,7 +73,6 @@ export default {
       .then(response => {
         console.log(response.data)
         this.recipe = response.data.recipe
-        console.log(this.weekNumber + ' ' + this.day + ' ' + this.recipe[0].name + ' ' + this.recipe[0].items + ' ' + this.recipe[0].instruction + ' ' + this.recipe[0].category)
       })
       .catch(error => {
         console.log(error.message)
@@ -84,26 +83,92 @@ export default {
       recipe: null,
       year: '',
       day: '',
-      weekNumber: ''
+      weekNumber: '',
+      existingDay: null
     }
   },
   methods: {
-    addToDay() {
-      Api.post('/profiles/' + localStorage.id + '/days', {
-        year: this.year,
-        name: this.day,
-        week: this.weekNumber,
-        recipes: this.recipe.recipe[0]
-      },
-      {
+    getWeekCal() {
+      Api.get('/profiles/' + localStorage.id + '/days?week=' + this.weekNumber + '&year=' + this.year, {
         headers: {
           Authorization: 'Bearer ' + localStorage.getItem('token')
         }
-      }).catch(error => {
-        alert('Warning: Recipe not added to day ' + error)
       })
-      console.log(this.weekNumber + ' ' + this.day + ' ' + this.recipe[0].name + ' ' + this.recipe[0].items + ' ' + this.recipe[0].instruction + ' ' + this.recipe[0].category)
-      this.$router.go()
+        .then(response => {
+          const weekCal = response.data.days
+          console.log(weekCal)
+          // console.log('---')
+          // console.log(this.existingDay)
+          // console.log('----')
+          for (const day of weekCal) {
+            if (day.name === this.day) {
+              this.existingDay = day
+              console.log(this.existingDay._id)
+            }
+          }
+          // console.log(this.existingDay)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    },
+    addToDay() {
+      this.getWeekCal()
+      console.log('----')
+
+      if (this.existingDay._id === 'none') {
+        console.log('newDay')
+        const recipeArrayDay = [this.$route.params.id]
+
+        Api.post('/profiles/' + localStorage.id + '/days', {
+          year: this.year,
+          name: this.day,
+          week: this.weekNumber,
+          recipes: recipeArrayDay
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+          }
+        }).then(response => {
+          if (response.status === 201) {
+            alert('Recipe was added to plan in week ' + this.weekNumber)
+          } else if (response.status === 404) {
+            alert('Warning: Recipe was not added to plan!')
+          }
+          console.log('post')
+          // console.log(this.existingDay.recipes.length)
+        }).catch(error => {
+          console.error(error)
+        })
+      } else {
+        this.existingDay.recipes.push(this.$router.id)
+        console.log('patch')
+        console.log(this.existingDay)
+        console.log(this.existingDay._id)
+
+        Api.patch('/profiles/' + localStorage.id + '/days/' + this.existingDay._id, {
+          year: this.year,
+          name: this.day,
+          week: this.weekNumber,
+          recipes: this.existingDay.recipes
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+          }
+        })
+          .then(response => {
+            if (response.status === 201) {
+              alert('Recipe was added to plan in week ' + this.weekNumber)
+            } else if (response.status === 404) {
+              alert('Warning: Recipe was not added to plan!')
+            }
+          })
+          .catch(error => {
+            console.error(error)
+          })
+      }
     },
     editRecipe() {
       this.$router.push(`/editRecipe/${this.$route.params.id}`)
